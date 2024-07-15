c3946e9e742294bb9de7620e350f8c7df8b75043
Lorena <128533577+LorenaSuarez0609@users.noreply.github.com>
2024-07-14 23:45:23 -0300

    subida

---
+++ foroHubAlura/src/main/java/com/foroHubAlura/foroHubAlura/ForoHubAluraApplication.java
@@ -0,0 +1,14 @@
+package com.foroHubAlura.foroHubAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubAluraApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ForoHubAluraApplication.class, args);
+	}
+
+}
