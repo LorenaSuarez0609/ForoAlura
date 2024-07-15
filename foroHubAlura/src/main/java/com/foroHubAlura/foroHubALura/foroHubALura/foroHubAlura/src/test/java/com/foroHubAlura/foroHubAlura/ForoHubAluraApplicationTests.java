536247e4d006b8e47e8126218a7945bd62176d72
Lorena <128533577+LorenaSuarez0609@users.noreply.github.com>
2024-07-14 23:46:43 -0300

    subida con cambios

---
+++ foroHubAlura/src/main/java/com/foroHubAlura/foroHubALura/foroHubALura/foroHubAlura/src/test/java/com/foroHubAlura/foroHubAlura/ForoHubAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foroHubAlura.foroHubAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
